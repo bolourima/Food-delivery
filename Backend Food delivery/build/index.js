89b044727ab07d7e038d608ebf4efe95c4c7d5ae
Bolormaa Ihkbayar <23LP2875@pinecones-MacBook-Air-2.local>
2024-02-22 19:01:14 +0800

    first

---
+++ Backend Food delivery/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello world!");
+const twoSum = (a, b) => {
+    return a + b;
+};
+console.log(twoSum(6, 9));
---
+++ Backend Food delivery/index.ts
@@ -0,0 +1,13 @@
+// console.log("Hello world!");
+
+// const twoSum = (a: number, b: number) => {
+//   return a + b;
+// };
+// console.log(twoSum(6, 10));
+
+// const trimText = (text: string) => {
+//   return text.trim;
+// };
+// console.log(trimText("     hayayaya     "));
+
+
